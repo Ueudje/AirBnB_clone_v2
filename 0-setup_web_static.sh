e30fe78d575670cb06009b57434b7f01ceab4fd5
Ueudje <classlol72@gmail.com>
2024-03-20 18:51:42 -0700

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # Prepare your web servers
 
-# if command not found or have some error install nginx, redirection output and err to null
+# if command not found or have some error install nginx,
+# redirection output and err to null
 if ! command -v nginx &> /dev/null; then
     sudo apt-get update -y && sudo apt-get install nginx -y
 fi
 # create all directory
-sudo mkdir -p /data/web_static/shared /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/data/web_static/releases/test
 sudo sh -c 'echo "Holberton School" > /data/web_static/releases/test/index.html'
 if [ -L "/data/web_static/current" ]; then
     sudo rm -r /data/web_static/current
@@ -25,12 +26,12 @@ server {
     location /redirect_me {
         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
     }
-    
+
     error_page 404 /404.html;
     location = /404.html {
         internal;
     }
-    
+
     location / {
       add_header X-Served-By "$(hostname)";
       try_files \$uri \$uri/ =404;
